d6cbca707df5c68ac0fe67b8072c0f6a10300e77
Arnav Raj <arnavraj22.official@gmail.com>
2021-05-15 20:16:15 +0530

    code splitting

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import App from "./App";
 import Logo from "./logo.svg";
 
 const root = document.getElementById("root");
-
 root.innerHTML = `
-<img src="${Logo}" alt ="build icon"/>
-<h1> Learning web tooling today</h1>
+<img src="${Logo}" alt="build icon" />
+<h1> learning web tooling today </h1>
 ${App({ name: "Arnav" })}
 `;
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,58 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const webpackConfig = {
-    entry: path.resolve(__dirname, "src", "index.js"),
+	entry: path.resolve(__dirname, "src", "index.js"),
 
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset"
+			}
+		]
+	},
 
-    // babble config *****
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"]
-                    }
-                }
-            },
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "vanillaJS app",
+			template: path.resolve(__dirname, "src", "index.html")
+		})
+	],
 
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
-            },
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				node_vendors: {
+					name: "vendor",
+					test: /[\\/]node_modules[\\/]/,
+					chunks: "all",
+					priority: 1
+				}
+			}
+		}
+	},
 
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset"
-            }
-        ]
-    },
-
-
-    mode: "production"
-}
+	mode: "production"
+};
 
 module.exports = webpackConfig;
